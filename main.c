388882b4d0cff64425054d64e87eecc49888af28
seclusioner <fileuseingbyds@gmail.com>
2024-06-09 09:54:38 +0800

    just for testing

--- main.c
+++ main.c
@@ -1,19 +1,18 @@
-#include <conio.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
+#include <time.h>
 
 #include "fft.h"
 #include "dsp.h"
+#include "complexlib.h"
 
+#define EPILSON 1e-10
 #define UNICODE
 
-float input[]={1.36849, 1.00912, 0.77214, -0.2554, -0.35185, -0.78784, -0.34128, -0.2047, 0.43879, 0.91979, 1.35083, 1.18693, 0.77584, 0.01244, -0.41674, -0.77935, -0.52584, 0.0619, 0.65222, 1.26387};
+#define ABS(n) n > 0 ? n : -n
 
-// -------------------------------
-#define SAMPLING_FREQ 10
-#define SAMPLES 5
-
-void print_arr(float *x, int N){
+void print_arr(double *x, int N){
     for(int i=0;i<N;i++){
         printf("%.2f ", x[i]);
     }
@@ -22,70 +21,104 @@ void print_arr(float *x, int N){
 
 void print_complex(dcomplex x, int mode){
     if(mode)
-        printf("(%f, %f)\n", x.re, x.im);
+        printf("(%10f, %10f)", x.re, x.im);
     else
-        printf("%f%+fi\n", x.re, x.im);
+        printf("%10f%10fi", x.re, x.im);
 }
 
-int main(void) {
-    // Complex Test
-    dcomplex a, b, c;
-    a.re = 1;
-    a.im = -1;
-    b.re = b.im = 2;
-
-    printf("a = ");print_complex(a, 0);
-    printf("b = ");print_complex(b, 0);
-
-    c = cadd(a, b);
-    printf("a + b = ");print_complex(c, 0);
-
-    c = csub(a, b);
-    printf("a - b = ");print_complex(c, 0);
-
-    c = cmul(a, b);
-    printf("a * b = ");print_complex(c, 0);
-
-    c = cdiv(a, b);
-    printf("a / b = ");print_complex(c, 0);
-
-    c = conjugate(a);
-    printf("a* = ");print_complex(c, 0);
-
-    printf("|a| = %f\n", magnitude(a));
-    printf("rad(a) = %f, deg(a) = %f\n", arg(a, 0), arg(a, 1));
-
-    printf("---------------\n");
-    // -----------------------------------------------
-    float x[SAMPLES]={0};
-    float h[SAMPLES]={0};
-    int N = SAMPLES;
-
-    for(int i=0;i<N;i++){
-        x[i] = h[i] = 1;
+void print_complexArray(dcomplex **x, int rows, int cols){
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            print_complex(x[i][j], 1);
+            printf(" ");
+        }
+        printf("\n");
     }
-    printf("Input Vector1: ");
-    print_arr(x, N);
-    printf("Input Vector2: ");
-    print_arr(h, N);
-    printf("---------------\n");
-    
-    float *yn;
+    printf("\n");
+}
 
-    yn = fftconvolve(x, h, SAMPLES, SAMPLES, &N);
-    printf("After FFT convolve:\n");
-    print_arr(yn, N);
-    printf("---------------\n");
-    
-    yn = convolve(x, h, SAMPLES, SAMPLES, &N);
-    printf("After convolve:\n");
-    print_arr(yn, N);
-    free(yn);
+/////////// testing ///////////
+double** Random2dArray(int rows, int cols) {
+    srand(time(NULL));
+    int min = 0;
+    int max = 255;
 
-    printf("---------------\n");
-    spectrum(input, sizeof(input)/sizeof(input[0]), SAMPLING_FREQ);
+    double** matrix = allocate2D_double(rows, cols); // H, W
+    if(matrix == NULL){
+        printf("Allocation failed!\n");
+        exit(1);
+    }
 
-	printf("\nPress any key to continue . . . ");
-	if (_getch()) printf("\n");
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            matrix[i][j] = rand() % (max - min + 1) + min;
+        }
+    }
+
+    return matrix;
+}
+
+
+int main(void) {
+
+    int flag = 0;
+    int ROWS, COLS;
+
+    ROWS = 2;
+    COLS = 3;
+
+    double** image = Random2dArray(ROWS, COLS); // H x W
+    dcomplex ** input = allocate2D_dcomplex(ROWS, COLS);
+    for(int i=0;i<ROWS;i++){
+        for(int j=0;j<COLS;j++){
+            //printf("%8.3f ", image[i][j]);
+            input[i][j].re = image[i][j];
+            printf("%8.3f ", input[i][j].re);
+        }
+        printf("\n");
+    }
+
+    //printf("\n----------fft2d---------------\n");
+    dcomplex **X = fft2d(input, ROWS, COLS, backward);
+    free2D_dcomplex(input, ROWS);
+
+    //printf("\n-----------shift----------\n");
+    spectrum_shift(X, ROWS, COLS);
+
+    //printf("\n-----------shift----------\n");
+    spectrum_shift(X, ROWS, COLS);
+
+    //printf("\n-----------ifft2d-------------\n");
+    dcomplex **result = ifft2d(X, ROWS, COLS, backward);
+
+    printf("\n");
+    for(int i=0;i<ROWS;i++){
+        for(int j=0;j<COLS;j++){
+            double error = result[i][j].re - image[i][j];
+            printf("%8.3f ", result[i][j].re);
+            if (error > EPILSON){
+                flag = 1;
+            }
+        }
+
+        printf("\n");
+
+        if(flag)
+            break;
+    }
+
+    ///////////////// Over /////////////////
+    free2D_double(image, ROWS);
+    free2D_dcomplex(X, ROWS);
+    free2D_dcomplex(result, ROWS);
+
+    printf("\n");
+    if(flag)
+        printf("???!\n");
+    else
+        printf("OK!\n");
+
+    printf("\nFinish\n");
+	system("pause");
 	return 0;
 }
